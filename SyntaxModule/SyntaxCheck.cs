b6ce7318656688d93a984a62d749672e7ac4365f
re-poem <BUCYUM@gmail.com>
2025-08-20 10:57:28 +0800

    修复对含有空格的note的语法检查

--- SyntaxModule/SyntaxCheck.cs
+++ SyntaxModule/SyntaxCheck.cs
@@ -75,7 +75,7 @@ namespace MajdataEdit.SyntaxModule
 
                 foreach (var s in simaiChart)
                 {
-                    string simaiStr = s.Replace("\n", "");
+                    string simaiStr = s.Replace("\n", "").Trim();
 
                     if (string.IsNullOrEmpty(s))
                         continue;
