ef76e5868d63c3b8fedf38ef40c3588fb9a27b83
re-poem <BUCYUM@gmail.com>
2025-08-25 10:48:17 +0800

    将runarg加入git避免切换分支错误

---
+++ MaiMuriDX/RunArg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajdataEdit.MaiMuriDX
+{
+    public class RunArg
+    {
+        public RunArg(string fumen, float offset, string trackPath, bool render) 
+        {
+            this.fumen = fumen;
+            this.offset = offset;
+            this.track = trackPath;
+            this.render = render;
+        }
+
+        public string fumen { get; set; }
+        public float offset { get; set; }
+        public string track { get; set; }
+        public bool render { get; set; }
+    }
+}
